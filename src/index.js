841c1d7e6ab98aa477420ac124f0820204b64cee
bearnyong <rhalsdud216@gmail.com>
2025-02-07 00:40:05 +0900

    center

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainPage from './pages/MainPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+          <Route path='/' element={<MainPage />} />
+        </Routes>
+    </BrowserRouter>
+  </>
 );
